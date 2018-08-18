e1e595da8106a60c8a1a6cb4a899037038aa78dc
William Roy <willjoroy@gmail.com>
2018-08-18 12:40:12 +0100

    Few Changes, got bump to work

--- main.lua
+++ main.lua
@@ -1,75 +1,65 @@
 local bump = require 'lib/bump'
 local world
 local player
-local platform
+local platform 
 
 function love.load()
-  world = bump.newWorld()
-  platform = {
-    x = 400,
-    y = 400,
-    width = 153,
-    height = 40,
-    img = nil
-  } 
-  player = {
-    x = 460,
-    y = 460,
-    w = 40,
-    h = 40,
-    dir = "right",
-    acc = 15,
-    base_speed = 8,
-    img = nil
-  }
-
-  player.img = love.graphics.newImage('assets/MC/character.png')
-  platform.img = love.graphics.newImage('assets/platforms/platformgrass.png')
-  
-  world:add(player, player.x, player.y, player.w, player.h)
+    platform = {
+        x = 300,
+        y = 300,
+        width = 153,
+        height = 40,
+        w = 153,
+        h = 40,
+        img = nil
+    } 
+    player = {
+        x = 460,
+        y = 460,
+        w = 40,
+        h = 40,
+        dir = "right",
+        speed = 130,
+        img = nil
+    }
+    world = bump.newWorld()
+    player.img = love.graphics.newImage('assets/MC/character.png')
+    platform.img = love.graphics.newImage('assets/platforms/platformgrass.png')
+    
+    world:add(player, player.x, player.y, player.w, player.h)
+    world:add(platform, platform.x, platform.y, platform.w, platform.h)
 end
 
 function love.update(dt)
-  if love.keyboard.isDown("right", "d") and player.x < 760 then
-    player.x = player.x + player.base_speed
-    player.dir = "right"
-  end
-  if love.keyboard.isDown("left", "a") and player.x > 0 then
-    player.x = player.x - player.base_speed
-    player.dir = "left"
-  end
-  if love.keyboard.isDown("up", "w") then
-    player.y = player.y - player.base_speed
-    player.dir = "up"
-  end
-  if love.keyboard.isDown("down", "s") then 
-    player.y = player.y + player.base_speed
-    player.dir = "down"
-  end  
-end
-
-function collision(object1, object2)
-  if object1.x > object2.x and object1.x < object2.x+object2.width then
-    if object1.y < object2.y then
-      return true
+    local future_x, future_y = player.x, player.y
+    player.speed = 200
+    if love.keyboard.isDown("right", "d") and player.x < 760 then
+        player.x = player.x + player.speed * dt
     end
-  end
-end
-
-function love.keypressed(key)
-  if key == "escape" then
-      love.event.push("quit")
-  end
-  if key == "x" then
-    print("x: "..player.x)
-    print("y: "..player.y)
-  end
+    if love.keyboard.isDown("left", "a") and player.x > 0 then
+        player.x = player.x - player.speed * dt
+    end
+    if love.keyboard.isDown("up", "w") then
+        player.y = player.y - player.speed * dt
+    end
+    if love.keyboard.isDown("down", "s") then 
+        player.y = player.y + player.speed * dt
+    end  
+    local newX, newY, cols, len = world:move(player, player.x, player.y)
+    player.x, player.y = newX, newY
 end
 
 function love.draw(dt)
-  love.graphics.draw(player.img, player.x, player.y)
-  love.graphics.draw(platform.img, platform.x, platform.y)
-  --('fill', ?, top right, ?, ?, top left, bottom)
-  love.graphics.polygon('fill', 0, 400, 1600, 400, 800, 399)
-  love.graphics.polygon('fill', 0, 500, 1600, 500, 0, 499)
+    love.graphics.draw(player.img, player.x, player.y)
+    love.graphics.draw(platform.img, platform.x, platform.y)
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.push("quit")
+    end
+    if key == "x" then
+        print("x: "..player.x)
+        print("y: "..player.y)
+    end
 end
