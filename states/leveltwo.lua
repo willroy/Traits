33d46cd0bd0e49299465acc7bd13884e1b6beadd
willroy <willjoroy@gmail.com>
2019-03-21 23:26:37 +0000

    more comments

--- states/leveltwo.lua
+++ states/leveltwo.lua
@@ -10,6 +10,7 @@ local function addBlock(x, y)
 end
 
 function enteredStatelvl2()
+  --player information
   player = {
     x = 600,
     y = 460,
@@ -20,25 +21,39 @@ function enteredStatelvl2()
     acc = 1,
     img = nil
   }
+  --house information
   house1 = {
+    --x and y coordinates for the location of the house
     x = 500,
     y = 240,
+    --width and height of the house image
     w = 294,
     h = 248,
+    --collision width and height for the size of the collision box
     c_w = 300,
     c_h = 248,
+    --roof height
     r_h = 72,
+    --images for roof and body
     img_body = nil,
     img_roof = nil
   }
+  --button information
   button1 = {
+    --x and y coordinate for the button location
     x = 200, 
     y = 500, 
+    --delay of the button
     delay = 7000, 
+    --whether or not the button is pressed
     pressed = false, 
+    --how many ticks have gone by since the button was pressed
     count = 0,
+    --the id of the button (unique identifier)
     id = 1,
+    --image of the button
     img = nil,
+    --the sound file the button plays on press
     note = love.audio.newSource("assets/AUDIO/Piano1.wav", "static")
   } 
   button2 = {
@@ -81,8 +96,10 @@ function enteredStatelvl2()
     img = nil
   } 
 
+  --list to keep track of order of button presses
   order = {}
 
+  --load images
   player.img = love.graphics.newImage('assets/PLAYER/PLAYER_v2_front.png')
   stone_tile = love.graphics.newImage('assets/TILE/TILE_stone.png')
   background = love.graphics.newImage('assets/TILE/TILE_backing.png')
@@ -95,11 +112,13 @@ function enteredStatelvl2()
   house1.img_body = love.graphics.newImage('assets/ENTRY/ENTRY_house1_body.png')
   house1.img_roof = love.graphics.newImage('assets/ENTRY/ENTRY_house1_roof.png')
   vines = love.graphics.newImage('assets/TILE/TILE_vines.png')
+  --load sound files for the buttons
   piano1 = love.audio.newSource("assets/AUDIO/Piano1.wav", "static")
   piano2 = love.audio.newSource("assets/AUDIO/Piano2.wav", "static")
   piano3 = love.audio.newSource("assets/AUDIO/Piano3.wav", "static")
   piano4 = love.audio.newSource("assets/AUDIO/Piano4.wav", "static")
 
+  --set the initial images for the buttons
   button1.img = button_up
   button2.img = button_up
   button3.img = button_up
@@ -107,8 +126,10 @@ function enteredStatelvl2()
   resetbutton.img = button_up_reset
 
   count = 0
+  --set the time for the level to 0 to start the timer
   countlvl2 = 0
 
+  --make mouse invisible on the window
   love.mouse.setVisible(true)
   world = bump.newWorld()
   right_x = 1260
@@ -116,17 +137,23 @@ function enteredStatelvl2()
   top_y = 0
   bot_y = 960
 
+  --set the bottom collision of the house higher so that the player can go up to door
   house1.c_h = house1.c_h - player.h
+  --add house and player to bump collision world
   world:add(house1.img_body, house1.x, house1.y+house1.r_h, house1.c_w, house1.c_h)
   world:add(player, player.x, player.y, player.w, player.h)
 end
 
 function updatelvl2(dt)
+  --add 1 to count each update loop, to track time
   countlvl2 = countlvl2 + 1
+  --set the future x and y to the current x an y
   local future_x, future_y = player.x, player.y
   player.speed = 6
   if love.keyboard.isDown("right", "d") and player.x < right_x then
+    --add to x coordinate to go right
     player.x = player.x + player.speed 
+    --set image to facing right when press right key
     player.img = love.graphics.newImage('assets/PLAYER/PLAYER_v2_right.png')
   end
   if love.keyboard.isDown("left", "a") and player.x > left_x then
@@ -142,38 +169,51 @@ function updatelvl2(dt)
     player.img = love.graphics.newImage('assets/PLAYER/PLAYER_v2_front.png')
   end 
 
+  --if the player is standing at the house's door, then check if the door is
+  --open
   if player.x > house1.x and player.x < house1.x + house1.w and player.y > 400 and player.y < 410 then
     if open == true then
-      print(open)
-      love.graphics.clear( )
+      --if it is open then return levelthree to go to next level
       return "levelthree", countlvl2
     end
   end
 
+  --if the player is at the top border then move them down
   if player.y < 300-player.h then
     player.y = player.y + player.speed    
   end
 
+  --check for player collisions and stop player progression if collision occurs
   local newX, newY, cols, len = world:move(player, player.x, player.y) player.x, player.y = newX, newY
+  --return leveltwo to keep going after loop
   return "leveltwo", countlvl2
 end
 
 function drawlvl2(dt)
+  --set a pointer to 0 and 300 for the coordinates of the first tile
   tile_x = 0
   tile_y = 300
+  --loop 138 time
   for i=138,0,-1 do
+    --every square in the grid on the level that is divisible by 9 or 4 is
+    --a stone
     if math.mod(i,9) == 0 or math.mod(i,4) == 0 then
       love.graphics.draw(stone_tile, tile_x, tile_y)
     else
+      --every other tile is a different stone tile
       love.graphics.draw(stone2_tile, tile_x, tile_y)
     end
+    --add 100 to tile_x, to go to next column of the background to draw
     tile_x = tile_x + 100
     if tile_x > 1300 then
+      --go to next row of background to draw
       tile_y = tile_y + 100
+      --reset x to first column
       tile_x = 0
     end
   end 
 
+  --if the order of the buttons is 1,4,3,2 then the house is open
   if order[1] == 1 then
     if order[2] == 4 then
       if order[3] == 3 then
@@ -185,19 +225,27 @@ function drawlvl2(dt)
     end
   end
 
+  --run the button function for each of the buttons in the entered state
+  --function
   drawbutton(button1)
   drawbutton(button2)
   drawbutton(button3)
   drawbutton(button4)
+  --different function for the reset button
   drawbuttonreset(resetbutton)
 
+  --set a pointer to 0 and 0 for the coordinates of the first tile of vines
   vinesx = 0
   vinesy = 0 
   for a=13,0,-1 do
+    --draw a vine image and then go to the next column until the whole width of
+    --the window is filled
     love.graphics.draw(vines, vinesx, vinesy)
     vinesx = vinesx + 300
   end 
 
+  --draw player and house (drawing roof over player so player can go behind
+  --house)
   love.graphics.draw(house1.img_body, house1.x, house1.y+house1.r_h)
   love.graphics.draw(player.img, player.x, player.y)
   love.graphics.draw(house1.img_roof, house1.x, house1.y)
@@ -209,6 +257,7 @@ function drawlvl2(dt)
 end
 
 function drawbuttonreset(rb)
+  --play the notes at certain intervals in the count timer
   if rb.count > 0 and rb.count < 100 then
     button1.note:play()    
   end
@@ -221,20 +270,27 @@ function drawbuttonreset(rb)
   if rb.count > 360 and rb.count < 460 then
     button2.note:play()    
   end
+  --if the time the button has been pressed is the max, then the button is no
+  --longer pressed
   if rb.count == rb.delay then
     rb.img = button_up_reset
     rb.pressed = false 
     order = {}
   end
+  --if the button is pressed then add 1 to the count each tick
   if rb.pressed == true then
     button1.count = 6950
     button2.count = 6950
     button3.count = 6950
     button4.count = 6950
     rb.count = rb.count + 1
+    --draw the button in the pressed position as the x and y coordinates of the
+    --button
     love.graphics.draw(rb.img, rb.x, rb.y)
   end
+  --if the player's position is on top of the button
   if player.x > rb.x-23 and player.x < (rb.x+100)-23 and (player.y+player.h) > rb.y and (player.y+player.h) < rb.y+100 then
+    --button has been pressed so set pressed to true
     rb.pressed = true
     rb.count = 0
     rb.img = button_down_reset
@@ -249,20 +305,30 @@ function drawbuttonreset(rb)
 end
 
 function drawbutton(b)
+  --play the note sound file for the button for 10 ticks
   if b.count < 10 and b.count > 0 then
     b.note:play()
   end
+  --if the time the button has been pressed is the max, then the button is no
+  --longer pressed
   if b.count == b.delay then
     b.img = button_up
     b.pressed = false 
     order = {}
   end
+  --if the button is pressed then add 1 to the count each tick
   if b.pressed == true then
     b.count = b.count + 1
+    --draw the button in the pressed position as the x and y coordinates of the
+    --button
     love.graphics.draw(b.img, b.x, b.y)
   end
+  --if the player's position is on top of the button
   if player.x > b.x-23 and player.x < (b.x+100)-23 and (player.y+player.h) > b.y and (player.y+player.h) < b.y+100 then
+    --button has been pressed so set pressed to true
     b.pressed = true
+    --if last button pressed was this button, then do nothing
+    --if another button has been pressed, then add the id to the order list.
     if order[#order] == b.id then
     else
       order[#order+1] = b.id
--- states/menu.lua
+++ states/menu.lua
@@ -1,12 +1,18 @@
+--entered state function to load images and set other things
 function enteredStatemenu()
+  --load the start button image
   start = love.graphics.newImage('assets/BUTTON/BUTTON_start.png')
+  --set the background colour to white
   love.graphics.setBackgroundColor(100,100,100)
 end
 
 function updatemenu(dt)
+  --x and y is the location of the mouse
   x, y = love.mouse.getPosition()
   local hover = false
 
+  --if the mouse is above the button, then the mouse is hovering over the
+  --button
   if x > 300 then
     if x < 500 then
       if y > 300 then
@@ -16,15 +22,18 @@ function updatemenu(dt)
       end
     end
   end
-    io.write(tostring(love.mouse.isDown(1)))
+  --if the mouse is over the button then test whether the mouse button is down
   if hover then
     if love.mouse.isDown(1) then
+      --go to level one if button is clicked
       return "levelone"
     end
   end
+  --if not clicked on button just return menu state
   return "menu"
 end
 
+--draw the start button at 500 500 xy coordinate
 function drawmenu(dt)
   love.graphics.draw(start, 500, 500)
 end
